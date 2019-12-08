59caba75880264982caf17ba884ea5169c961a19
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2019-12-08 22:40:04 +0530

    graphql works

--- server/tests/graphql2.js
+++ server/tests/graphql2.js
@@ -5,10 +5,11 @@ const gql = require('graphql-tag')
 
 const graphqlHTTP = require('express-graphql')
 
-const mongoose = require('./node_modules/mongoose/index')
+const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/sports', {
 	useUnifiedTopology: true,
-	useNewUrlParser: true
+	useNewUrlParser: true,
+	useFindAndModify: false
 })
 
 // GraphQL initialization
@@ -21,17 +22,115 @@ const typeDefs = gql`
 		HouseName: String
 		Info: String
 	}
+
+	input HouseInput {
+		HouseName: String!
+		Info: String
+	}
+
+	input PlayerInput {
+		PlayerName: String
+		House: String
+		Course: String
+		Year: String
+		About: String
+	}
+
+	type Player {
+		_id: ID!
+		PlayerName: String
+		House: House
+		Course: String
+		Year: String
+		About: String
+	}
+
 	type Query {
 		houses: [House]
+		players: [Player]
+		player(name: String): [Player]
+	}
+
+	type Mutation {
+		createPlayer(input: PlayerInput): Player
+		updatePlayer(id: ID!, input: PlayerInput): Player
+		deletePlayer(id: ID!): Player
 	}
 `
+
+let HouseModel = mongoose.model(
+	'house',
+	require('../schemas/houseSchema').HouseSchema
+)
+
+let PlayerModel = mongoose.model(
+	'player',
+	require('../schemas/playerSchema').PlayerSchema
+)
+
+const getHouseId = async houseName => {
+	try {
+		const house = HouseModel
+		const result = await house.findOne({ HouseName: houseName })
+		return result['_id']
+	} catch (err) {
+		console.log(err)
+		throw new Error('House not found')
+	}
+}
 
 const resolvers = {
 	Query: {
 		async houses() {
-			return await mongoose
-				.model('house', require('../schemas/houseSchema').HouseSchema)
-				.find()
+			return await HouseModel.find()
+		},
+		async players() {
+			return await PlayerModel.find().populate('House')
+		},
+		async player(root, args) {
+			return await PlayerModel.find({ PlayerName: args.name }).populate(
+				'House'
+			)
+		}
+	},
+	Mutation: {
+		async createPlayer(_, args) {
+			try {
+				input = args.input
+				input.House = await getHouseId(input.House)
+				const player = new PlayerModel(input)
+				const result = await player.save()
+				return result
+			} catch (err) {
+				console.log(err)
+				return err
+			}
+		},
+		async updatePlayer(_, args) {
+			try {
+				input = args.input
+				if (args.input.House) {
+					args.input.House = await getHouseId(args.input.House)
+				}
+				return await PlayerModel.findByIdAndUpdate(
+					{ _id: args.id },
+					args.input
+				).populate('House')
+			} catch (err) {
+				console.log(err)
+				return err
+			}
+		},
+		async deletePlayer(_, args) {
+			try {
+				input = args.input
+				return await PlayerModel.findByIdAndDelete({
+					_id: args.id
+				}).populate('House')
+			} catch (err) {
+				console.log(err)
+				return err
+			}
 		}
 	}
 }
@@ -48,5 +147,12 @@ app.use(
 		graphiql: true
 	})
 )
+
+async function execute(name) {
+	const c = await getHouseId('Keen Kickers')
+	console.log(c)
+}
+
+// execute('Sara Lake')
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
